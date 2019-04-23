d519e841c35d39bb7bd5ee655e86a7b7df98f93c
BegumTop <begumtop@umich.edu>
2019-04-23 12:17:30 -0400

    merge model files

---
+++ code/merge_files.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Author: Begum Topcuoglu
+# Date: 2018-02-13
+#
+#######################################################################################
+# This script will:
+#   1. Take single .csv files that have the model result for one datasplit
+#   2. Combine them together to have the results for 100 datasplits in one .csv file
+#   3. We don't keep the header of each file when combined but only once.
+
+# In the end, the combined_best file must be 101 lines. 1st line is the header and the 100 lines have the data of 100 files$
+#             the combined_all file must have 100*(hyper-parameter number)+1 lines.
+########################################################################################
+
+SEARCH_DIR=code/learning/data/temp
+FINAL_DIR=code/learning/data/process
+
+# Keep the first line of File1 and remove the first line of all the others and combine
+
+for model in "L2_Logistic_Regression"
+do
+  	head -1 $SEARCH_DIR/all_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_all_hp_results_"$model".csv; tail -n +2 -$
+        head -1 $SEARCH_DIR/best_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_best_hp_results_"$model".csv; tail -n +2$
+        head -1 $SEARCH_DIR/all_imp_features_non_cor_results_"$model"_1.csv > $SEARCH_DIR/combined_all_imp_features_non_cor$
+        head -1 $SEARCH_DIR/all_imp_features_cor_results_"$model"_1.csv > $SEARCH_DIR/combined_all_imp_features_cor_results$
+        head -1 $SEARCH_DIR/walltime_"$model"_1.csv  > $SEARCH_DIR/walltime_"$model".csv; tail -n +2 -q $SEARCH_DIR/walltim$
+
+        mv $SEARCH_DIR/walltime_"$model".csv $FINAL_DIR/walltime_"$model".csv
+        mv $SEARCH_DIR/combined_all_hp_results_"$model".csv $FINAL_DIR/combined_all_hp_results_"$model".csv
+        mv $SEARCH_DIR/combined_best_hp_results_"$model".csv $FINAL_DIR/combined_best_hp_results_"$model".csv
+        mv $SEARCH_DIR/combined_all_imp_features_non_cor_results_"$model".csv $FINAL_DIR/combined_all_imp_features_non_cor_$
+        mv $SEARCH_DIR/combined_all_imp_features_cor_results_"$model".csv $FINAL_DIR/combined_all_imp_features_cor_results_$
+done
+
