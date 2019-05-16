743b57dfac589f9ff61197e08cea0d2c3838d089
akhagan <akhagan@umich.edu>
2019-05-16 11:49:55 -0400

    work on binning institutions

--- code/load_data.R
+++ code/load_data.R
@@ -19,15 +19,67 @@ data <- left_join(manu_data, reviews_data, by = c("random.manu.num", "grouped.ra
 carnegie_class <- read_csv("data/carnegie_class.csv") %>% 
   select(name, city, state, Basic)
 
+us_industries <- read_csv("data/us_industries.csv") %>% 
+  pull(institution.name) %>% 
+  str_to_lower(.) %>% unique()
+
+fed_labs <- read_csv("data/fed_state_labs.csv") %>% 
+  pull(institution.name) %>% 
+  str_to_lower(.) %>% unique()
+
+R1 <- carnegie_class %>% 
+  filter(Basic == "Doctoral Universities: Very High Research Activity") %>% 
+  pull(name) %>% str_to_lower(.)
+
+R2 <- carnegie_class %>% 
+  filter(Basic == "Doctoral Universities: High Research Activity") %>% 
+  pull(name) %>% str_to_lower(.)
+
+low_research <- carnegie_class %>% 
+  filter(str_detect(Basic, "High Research") == FALSE) %>% 
+  pull(name) %>% str_to_lower(.)
+
 test <- data %>% 
-  left_join(., carnegie_class, by = c("institution.y" = "name")) %>% 
+  mutate(institution.y = str_to_lower(institution.y)) %>% 
   mutate(
     US.inst = if_else(country.y == "United States", "yes", "no"),
     US.inst.type = case_when(
-    !is.na(state) & Basic == "Doctoral Universities: Very High Research Activity" ~ "R1", 
-    !is.na(state) & Basic == "Doctoral Universities: High Research Activity" ~ "R2",
-    !is.na(state) & !is.na(Basic) ~ "Low research"))
+      institution.y %in% R1 | str_detect(institution.y, ) ~ "R1",
+      institution.y %in% R2 ~ "R2",
+      institution.y %in% low_research ~ "Low research",
+      institution.y %in% fed_labs ~ "Federal",
+      institution.y %in% us_industries ~ "Industry"
+))
 
-US_ungrouped <- test %>% filter(US.inst == "yes") %>% filter(author.corres == TRUE) %>% select(institution.y, US.inst.type) %>% group_by(US.inst.type, institution.y) %>% summarise(n = n()) %>% filter(is.na(US.inst.type))
+industry <- c("abbott", "abbvie", "accelerate", "achaogen", "achillion", "amgen",
+              "allergan", "amplyx", "pharmaceuticals", "astrazeneca", "avidbiotics",
+              "biopharma", "diagnostics", "bd", "beckman coulter", "bio rad", 
+              "biorad", "biomerieux", "myers squibb", "chembio", "cellex",
+              "therapeutics", "dupont", "genentech")
 
-write_csv(US_ungrouped, path = "data/ungrouped_inst.csv")
+govt <- c("usda", "agricultural research service", "air force", 
+          "regional healthcare", "state health", "armed forces", "nasa", 
+          "department of public health", "fda", "cdc", "national cancer institute", 
+          "food and drug administration", "cleveland clinic", "health department",
+          "homeland security", "veterans affairs", "environmental protection agency",
+          "epa", "national lab")
+
+r1 <- c("harvard", "binghamton", "boston children's hospital",
+        "george washington", "brigham and women's", "boston university", 
+        "cornell university", "cuny graduate", "dana farber", "dartmouth",
+        "broad institute", "case western", "children's hospital", "children's mercy",
+        "university of michigan", "clemson", "columbia university", "duke",
+        "emory university", "auburn", "georgetown university")
+
+r2 <- c("cuny city", "east tennessee state", "baylor", "eastern michigan")
+
+low <- c("baptist health", "california state")
+
+US_ungrouped <- test %>% filter(US.inst == "yes") %>% 
+  filter(author.corres == TRUE) %>% 
+  select(institution.y, US.inst.type) %>% 
+  group_by(US.inst.type, institution.y) %>% 
+  filter(is.na(US.inst.type)) %>% 
+  summarise(n = n()) %>% View()
+
+#write_csv(US_ungrouped, path = "data/ungrouped_inst.csv")
