57a1797ecc98f9d457a4c0400d445e2aa22667c3
akhagan <akhagan@umich.edu>
2019-05-20 16:29:51 -0400

    add category to binning institutions

--- code/load_data.R
+++ code/load_data.R
@@ -1,3 +1,4 @@
+#setup (libraries & data import)----
 library(tidyverse)
 library(lubridate)
 
@@ -16,6 +17,7 @@ data <- left_join(manu_data, reviews_data, by = c("random.manu.num", "grouped.ra
   select(-role.x) %>% #drop unneeded role.x column (b/c all reviewer)
   filter(year(submitted.date) >= "2011") #drop anything submitted in 2011
 
+#carnegie classifications (R1, R2 research etc)----
 carnegie_class <- read_csv("data/carnegie_class.csv") %>% 
   select(name, city, state, Basic)
 
@@ -43,8 +45,13 @@ industry <- c("abbott", "abbvie", "accelerate", "achaogen", "achillion", "amgen"
               "allergan", "amplyx", "pharmaceuticals", "astrazeneca", "avidbiotics",
               "biopharma", "diagnostics", "bd", "beckman coulter", "bio rad", 
               "biorad", "biomerieux", "myers squibb", "chembio", "cellex",
-              "therapeutics", "dupont", "genentech", " inc", "pharma", 
-              "biomérieux", "new england biolabs", "corporation") %>% 
+              "therapeutics", "dupont", "genentech", " inc", "pharma", "merck",
+              "biomérieux", "new england biolabs", "corporation", "consultant", 
+              "jmi laboratories", "novartis", "the medicines company", "pfizer",
+              "glaxosmithkline", "genesis biotechnology group", "sanofi",
+              "laboratories", "profectus biosciences", "emergent biosolutions",
+              "integral molecular", "jackson laboratory", "roche",
+              "neomed") %>% 
   paste0(., collapse = "|")
 
 govt <- c("usda", "agricultural research service", "air force", "nih", "edgewood",
@@ -54,13 +61,42 @@ govt <- c("usda", "agricultural research service", "air force", "nih", "edgewood
           "homeland security", "veterans affairs", "environmental protection agency",
           "epa", "national lab", "centers for disease control", " va ", "army", "llc",
           "state public health laboratory", "state public health lab", "veterans",
-          "rocky mountain lab") %>% 
+          "rocky mountain lab", "usamriid", "niaid", "national institute of",
+          "va medical center", "uniformed services university", "nci ",
+          "national animal disease center", "national institute of allergy and",
+          "united states geological survey", "naval medical research",
+          "wadsworth center, new york state dept health", "niddk",
+          "national oceanic and atmospheric administration", "united states") %>% 
   paste0(., collapse = "|")
 
-r1 <- c("harvard", "binghamton", "boston children's hospital", "cornell",
+medical <- c("mayo clinic", "scripps research", "research hospital", 
+             "fred hutchinson", "massachusetts general hospital", 
+             "beth israel deaconess medical center", "hospital", "children's mercy",
+             "memorial sloan kettering", "school of medicine", "cedars sinai",
+             "medical school", "medicine at ucla", "arkansas for med", 
+             "medical center", "medical uni", "college of medicine",
+             "louisana state university health sciences", "cancer center",
+             "center for infectious disease research", "primate research center",
+             "rosalind franklin university", "icahn school of medicine",
+             "university of tennessee health science center", "research institute",
+             "anschutz medical campus", "health system", "institute",
+             "texas health science", "utmb", "texas medical branch", "ohsu",
+             "sch of medicine", "utmb", "texas medical branch", "uthscsa",
+             "oklahoma health sci", "medical college", "national jewish health",
+             "oklahoma hsc", "oklahoma-health sci", "massacussetts med",
+             "oregon health and science university", "texas health science",
+             "university of tennessee health science center", "lsuhsc",
+             "international aids vaccine initiative", "woods hole",
+             "aaron diamond aids research center", "school of med",
+             "oklahoma medical research foundation", "marine biological laboratory",
+             "scripps institution") %>% 
+  paste0(., collapse = "|")
+
+r1 <- c("harvard", "binghamton", "cornell", "ucsd", "uc davis", "tulane university",
         "george washington", "brigham and women's", "boston university", 
         "cornell university", "cuny graduate", "dana farber", "dartmouth",
-        "broad institute", "case western", "children's hospital", "children's mercy",
+        "broad institute", "case western", "mit", "university of mississippi",
+        "lousiana state university, baton rouge", "stanford", "uc berkeley", 
         "university of michigan", "clemson", "columbia university", "duke",
         "emory university", "auburn", "georgetown university", "wisconsin madison",
         "wisconsin–madison", "university of minnesota", "ohio state", "drexel",
@@ -87,7 +123,8 @@ r1 <- c("harvard", "binghamton", "boston children's hospital", "cornell",
         "pittsburgh", "of rochester", "of south carolina", "south florida$", 
         "ity of southern cali", "utah", "of virginia", "wisconsin milwaukee",
         "wisconsin–milwaukee", "virginia commonwealth", "washington state uni",
-        "tennessee", "tenessee", "of mass", "^university of missouri") %>% 
+        "tennessee", "tenessee", "of mass", "^university of missouri",
+        "university at buffalo", "georgia tech") %>% 
   paste0(., collapse = "|")
 
 r2 <- c("cuny city", "east tennessee state", "baylor", "eastern michigan",
@@ -109,7 +146,7 @@ r2 <- c("cuny city", "east tennessee state", "baylor", "eastern michigan",
         "university of texas health science center at san antonio", "akron",
         "merced", "of maine", "wilmington", "ity of north dakota", "puerto rico",
         "of rhode island", "uc san diego", "south alabama", "south dakota",
-        "toledo", "vermont", "wake forest", "wright state", "des moines",
+        "toledo", "vermont", "wake forest", "wright state", "des moines", "etsu",
         "university of tennessee at chattanooga", "missouri–st louis") %>% 
   paste0(., collapse = "|")
 
@@ -119,38 +156,23 @@ low <- c("baptist health", "california state", "university of washington",
          "texas women's university", "st john's university", "edwardsville",
          "california, sf", "california san francisco", "detroit mercy", "hofstra",
          "pacific univeristy", "pace university", "long island univeristy", 
-         "creighton university", "augusta university", "icahn school of medicine",
-         "arizona state university west campus", "mayo clinic college of medicine",
-         "louisana state university health sciences", "rosalind franklin university",
-         "medical univerity of south carolina", "midwestern university",
-         "medical college oif wisconsin", "medical college of wisconsin",
-         "oregon health and science university", "rush university", 
-         "suny downstate", "niversity of tennessee health science center",
-         "texas health science", "utmb", "texas medical branch",
-         "arkansas for med", "nebraska medical center", "oklahoma health sci",
-         "oklahoma hsc", "oklahoma-health sci", "ut southwestern", "utsouthwestern",
-         "texas southwestern", "suny upstate", "weill cornell", "massacussetts med",
-         "maryland baltimore", "maryland school of medicine", 
-         "maryland sch of medicine") %>% 
+         "creighton university", "augusta university", "maryland baltimore",
+         "arizona state university west campus", "midwestern university",
+         "rush university", "suny downstate", "weill cornell", "texas southwestern",
+         "arkansas for med", "ut southwestern", "utsouthwestern", "ucsf",
+         "suny upstate", "new york at buffalo", "hunter college",
+         "suny buffalo", "california polytechnic ") %>% 
   paste0(., collapse = "|")
 
-test <- data %>% 
+#bin US institutions----
+data <- data %>% 
   mutate(institution.y = str_to_lower(institution.y)) %>% 
   mutate(
     US.inst = if_else(country.y == "United States", "yes", "no"), #deal w. multiple inst?
     US.inst.type = case_when(
-      institution.y %in% R1 | str_detect(institution.y, r1) ~ "R1",
-      institution.y %in% R2 | str_detect(institution.y, r2) ~ "R2",
-      institution.y %in% low_research | str_detect(institution.y, low) ~ "Low research",
-      institution.y %in% fed_labs | str_detect(institution.y, govt) ~ "Federal",
-      institution.y %in% us_industries | str_detect(institution.y, industry) ~ "Industry"
-    ))
-
-US_ungrouped <- test %>% filter(US.inst == "yes") %>% 
-  filter(author.corres == TRUE) %>% 
-  select(institution.y, US.inst.type) %>% 
-  group_by(US.inst.type, institution.y) %>% 
-  filter(is.na(US.inst.type)) %>% 
-  summarise(n = n()) %>% arrange(desc(n))
-
-#write_csv(US_ungrouped, path = "data/ungrouped_inst.csv")
+      institution.y %in% us_industries | str_detect(institution.y, industry) ~ "Industry Research",
+      institution.y %in% R1 | str_detect(institution.y, r1) ~ "R1 Institution",
+      str_detect(institution.y, medical) ~ "Institute or Medical School",
+      institution.y %in% R2 | str_detect(institution.y, r2) ~ "R2  Institution",
+      institution.y %in% low_research | str_detect(institution.y, low) ~ "Low Research  Institution",
+      institution.y %in% fed_labs | str_detect(institution.y, govt) ~ "Federal Research"))
