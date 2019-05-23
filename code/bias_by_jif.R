e0adc1327c26328668a72cc0ed47586705ab525b
akhagan <akhagan@umich.edu>
2019-05-23 16:18:39 -0400

    work on analysis by jif

--- code/bias_by_jif.R
+++ code/bias_by_jif.R
@@ -1,9 +1,12 @@
-hist_jif <- read_csv("data/jif_2010_17.csv") %>% 
-  separate("Journal;Impact Factor;Year", 
-           into = c("Journal", "JIF", "year"), sep = ";") %>% 
-  filter(JIF != "n/a") %>% 
-  group_by(Journal) %>% 
-  summarise(n = n(), sum.JIF = sum(as.numeric(JIF)))
+acc_data <- data %>% filter(published == "yes") %>% 
+  filter(EJP.decision != "NA") %>% 
+  mutate(gender = fct_explicit_na(gender, na_level = "none")) %>% 
+  filter(role == "author" & author.corres == "TRUE") %>% 
+  select(version, grouped.random, random.manu.num, gender, 
+         EJP.decision, journal, contains("days"),
+         num.versions) %>% distinct()
+
+#add hist_jif back
 
 max_jif <- read_csv("data/max_jif.csv") %>% 
   separate("Journal;max.JIF;year.founded", 
@@ -11,4 +14,16 @@ max_jif <- read_csv("data/max_jif.csv") %>%
   left_join(., hist_jif, by = "Journal") %>% 
   mutate(years.old = 2019 - as.numeric(year.founded),
          avg.JIF = (sum.JIF/n),
-         percieved.JIF = avg.JIF + as.numeric(max.JIF) + years.old)
+         percieved.JIF = avg.JIF + as.numeric(max.JIF) + years.old) %>% 
+  select(Journal, avg.JIF, percieved.JIF)
+
+jif_acc_data <- left_join(acc_data, max_jif, 
+                          by = c("journal" = "Journal"))
+
+jif_acc_data %>% 
+  filter(gender != "none") %>% 
+  ggplot()+
+  geom_density(aes(x = percieved.JIF, fill = gender), alpha = 0.5)+
+  facet_wrap(~EJP.decision)+
+  scale_fill_manual(values = gen_colors)+
+  my_theme_leg_horiz
