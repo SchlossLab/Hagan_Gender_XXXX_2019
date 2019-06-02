8e1ab436de26dd016998acc0d313b17e44d5e3b8
akhagan <akhagan@umich.edu>
2019-06-02 09:56:43 -0400

    condense to single plot comparing differences in percent editorial rejection

--- code/editorial_rejections.R
+++ code/editorial_rejections.R
@@ -13,30 +13,35 @@ ASM_summary <- bias_data %>%
   select(gender, grouped.random) %>% distinct() %>% 
   group_by(gender) %>% summarise(n = n())
 
-ed_rejs %>% 
+ASM_ed_rej <- ed_rejs %>% 
   group_by(gender) %>% 
   summarise(n = n()) %>% 
   mutate(prop_rej = get_percent(n, ASM_summary$n)) %>% 
-  ggplot()+
-  geom_col(aes(x = gender, y = prop_rej, fill = gender))+
-  scale_fill_manual(values = gen_colors)+
-  labs(x = "Gender", y = "Percent Editorial Rejections")+
-  my_theme_horiz
+  select(-n) %>% 
+  spread(key = gender, value = prop_rej) %>% 
+  mutate(performance = male - female) %>% 
+  cbind(journal = "All Combined", .) %>% as_tibble()
 
 #percent of submissions that are editorial rejections by gender & journal
 journal_summary <- bias_data %>% 
   select(journal, gender, grouped.random) %>% distinct() %>% 
   group_by(journal, gender) %>% summarise(total = n())
 
-ed_rejs %>% 
+ed_rejections_A <- ed_rejs %>% 
   group_by(journal, gender) %>% 
   summarise(n = n()) %>% 
   left_join(., journal_summary, by = c("journal", "gender")) %>% 
   distinct() %>% 
-  mutate(prop_rej = get_percent(n, total)) %>%
+  mutate(prop_rej = get_percent(n, total)) %>% 
+  select(-n, -total) %>% 
+  spread(key = gender, value = prop_rej) %>% 
+  mutate(performance = male - female) %>% as_tibble() %>% 
+  rbind(ASM_ed_rej) %>% 
   ggplot() +
-  geom_col(aes(x = gender, y = prop_rej, fill = gender)) + 
-  facet_wrap(~journal)+
-  scale_fill_manual(values = gen_colors)+
-  labs(x = "Gender", y = "Percent Editorial Rejections")+
+  geom_col(aes(x = journal, y = performance, fill = performance)) + 
+  #scale_fill_manual(values = gen_colors)+
+  coord_flip()+
+  gen_gradient+
+  labs(x = "Journal", 
+       y = "\nDifference in Percent Editorial Rejections\n(Men - Women)")+
   my_theme_horiz
