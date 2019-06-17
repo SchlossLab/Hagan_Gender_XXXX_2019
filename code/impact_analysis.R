b12795efb0a43f83f32763af45e19b005f224cfc
akhagan <akhagan@umich.edu>
2019-06-17 15:30:28 -0400

    update citation/usage data

--- code/impact_analysis.R
+++ code/impact_analysis.R
@@ -1,12 +1,12 @@
 #setup----
-cites <- read_csv("../data/cites.csv") %>% 
+cites <- read_csv("../data/cites_1.csv") %>% 
   select(`Article DOI (article_metadata)`, `Published Months`, 
          `Article Date of Publication (article_metadata)`, `Citation Date`, Cites, `Mendeley Saves`) %>% 
   group_by(`Article DOI (article_metadata)`,`Published Months`, 
            `Article Date of Publication (article_metadata)`, `Mendeley Saves`) %>% 
   summarise(Cites = sum(Cites))
 
-usage <- read_csv("../data/usage.csv") %>% 
+usage <- read_csv("../data/usage_1.csv") %>% 
   select(`Article DOI (article_metadata)`, `Total Abstract`, `Total HTML`, `Total PDF`)
 
 c_u_data <- full_join(cites, usage, by = "Article DOI (article_metadata)") %>% distinct()
@@ -36,22 +36,16 @@ impact_data <- bias_data %>%
             by = c("doi" = "Article DOI (article_metadata)")) %>% 
   distinct() %>% 
   filter(!is.na(`Article Date of Publication (article_metadata)`)) %>% 
-  left_join(., max_jif, by = c("journal" = "Journal")) %>% 
-  mutate(`Total Reads` = `Total HTML` + `Total PDF`) %>% 
-  gather(`Mendeley Saves`:`Total Reads`, 
+  left_join(., max_jif, by = c("journal" = "Journal")) %>%
+  mutate(`Total Reads` = `Total HTML` + `Total PDF`,
+         log.avg.JIF = log10(avg.JIF),
+         `Cites/log.JIF` = Cites-log.avg.JIF) %>% 
+  gather(`Mendeley Saves`:`Cites/log.JIF`, 
          key = measure.name, value = measure.value) %>% 
   mutate(value.per.month = measure.value/`Published Months`)
 
 #plots----
-factors_C <- plot_impact_data("Cites", 5)
-
-#plot_impact_data("Total PDF", 200)
-
-#plot_impact_data("Total HTML", 200)
-
-#plot_impact_data("Total Abstract", 200)
-
-#plot_impact_data("Mendeley Saves", 5)
+factors_C <- plot_impact_data("Cites", 3)
 
 factors_D <- plot_impact_data("Total Reads", 300)
 
