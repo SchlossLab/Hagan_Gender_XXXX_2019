8670060c05154ae06262e5e28239f50e58cfa0e2
akhagan <akhagan@umich.edu>
2019-07-22 16:13:17 -0400

    script to use desc_matrix to test 15 - 100 discipline clusters

--- code/bias/clustered_disciplines/kmeans.R
+++ code/bias/clustered_disciplines/kmeans.R
@@ -1,38 +1,20 @@
 library(tidyverse)
 library(NbClust)
 
-descriptors <- read_csv("data/valued_descriptors.csv")
+desc_matrix <- read_csv("data/desc_matrix.csv")
 
-journ_desc <- descriptors %>% 
-  filter(type == "journal") %>% 
-  select(-word, -type) %>% distinct() %>% 
-  rename("journal.val" = "value")
+set.seed(1234)
 
-cat_desc <- descriptors %>% 
-  filter(type == "category") %>% 
-  select(-word, -type) %>% distinct() %>% 
-  rename("category.val" = "value")
+nc <- NbClust(desc_matrix, min.nc=15, max.nc=100, method="kmeans")
 
-max_desc <- descriptors %>% group_by(grouped.random) %>% summarise(n = n()) %>% arrange(desc(n))
-#limit to papers with at least 8 unique descriptors, restrict to 8
+print("NbClust complete")
 
-keyword_desc <- descriptors %>% 
-  filter(is.na(type)) %>% 
-  select(-type, -word) %>% distinct() %>% 
-  rename("keyword.val" = value)
+save.image()
 
-manus <- descriptors %>% pull(grouped.random) %>% unique()
+clust_test <- table(nc$Best.n[1,])
 
-eight_desc <- map_dfr(manus, function(x){
-  
-  this_manu <- keyword_desc %>% filter(grouped.random == x) %>% distinct()
-  
-  df <- this_manu %>% head(n = 8) %>% select(-grouped.random) %>% 
-      t() %>% as.tibble() %>% cbind(grouped.random = x, .)
-  
-  return(df)
-}) %>% filter(!is.na(V8))
+save.image()
 
-#set.seed(1234)
-#nc <- NbClust(df, min.nc=2, max.nc=15, method="kmeans")
-#table(nc$Best.n[1,])
+write_csv(clust_test, "data/clust_test_2019_22_07.csv")
+
+print("clust output saved")
