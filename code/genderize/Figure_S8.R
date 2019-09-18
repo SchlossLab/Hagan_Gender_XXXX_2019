dcc73a2c4df5d0f5b33db5a1b43c5146f29c34f2
akhagan <akhagan@umich.edu>
2019-09-18 15:21:01 -0400

    merge supplementary figures

--- code/genderize/Figure_S8.R
+++ code/genderize/Figure_S8.R
@@ -1,76 +1,87 @@
-#full b_c dataset w. country data
-country_impact_data <- read_csv("../data/B_C_auth_country_genderize_join_nosp.csv") %>% 
-  filter(!is.na(country)) %>% #drop data w/o associated country
-  filter(!is.na(code)) %>% #drop data w countries that aren't supported by genderize
-  filter(str_detect(country, "/") == FALSE) %>% #drop data w. 2+ countries
-  mutate(predicted = if_else(is.na(genderize.gender) == TRUE, "no", "yes") %>% as.factor()) %>% #add binary factor for prediction
+library(png)
+library(grid)
+
+#equation----
+equation <- readPNG("../submission/impact_equation.png")
+
+equ_ger <- grid::rasterGrob(equation, interpolate=TRUE)
+
+equ_plot <- ggdraw() + draw_grob(equ_ger)
+
+#full ASM dataset w. country data----
+ASM_country_impact_data <- people_data %>% 
+  select(random.person.id, gender, country) %>% distinct() %>% 
+  mutate(predicted = if_else(is.na(gender) == TRUE, "no", "yes") %>% as.factor()) %>% #add binary factor for prediction
   mutate(country = as.factor(country)) %>% #convert countries to factors
   distinct() #unique entries
 
-#summary stats----
-country_list <- country_impact_data %>% 
+#summary stats---
+ASM_country_list <- ASM_country_impact_data %>% 
   pull(country) %>% unique() #all countries
 
-num_countries <- length(country_list) #number of countries
+ASM_num_countries <- length(ASM_country_list) #number of countries
 
 #df of names w. country data that weren't assigned a gender
-na_obs <- country_impact_data %>% 
-  filter(is.na(genderize.gender) & !is.na(code)) %>% distinct()
+ASM_na_obs <- ASM_country_impact_data %>% 
+  filter(is.na(gender)) %>% distinct()
 
-num_obs <- country_impact_data %>% nrow()
+ASM_num_obs <- ASM_country_impact_data %>% nrow()
 
-num_na_countries <- na_obs %>% pull(country) %>% 
+ASM_num_na_countries <- ASM_na_obs %>% pull(country) %>% 
   unique() %>% length() #countries associated w. na-gender names
 
-num_na_obs <- na_obs %>% nrow() #total na-gender names
+ASM_num_na_obs <- ASM_na_obs %>% nrow() #total na-gender names
 
 #total names assigned gender
-num_predicted <- country_impact_data %>% 
-  filter(!is.na(genderize.gender) & !is.na(code)) %>% nrow()
+ASM_num_predicted <- ASM_country_impact_data %>% 
+  filter(!is.na(gender)) %>% nrow()
 
 #percent of names w. country data assigned "na" gender
-percent_unpredicted <- get_percent(num_na_obs, num_obs)
+ASM_percent_unpredicted <- get_percent(ASM_num_na_obs, ASM_num_obs)
 
 #how many names from each country had genders predicted?----
-predictions_by_country <- table(country_impact_data$country, #count prediction values for each country
-                                country_impact_data$predicted) %>% 
+ASM_predictions_by_country <- table(ASM_country_impact_data$country, #count prediction values for each country
+                                    ASM_country_impact_data$predicted) %>% 
   as_tibble(., .name_repair = "universal") #convert to tibble
 
-colnames(predictions_by_country)[1:2] <- c("country", "prediction")
+colnames(ASM_predictions_by_country)[1:2] <- c("country", "prediction")
 
-predictions_by_country <- predictions_by_country %>% 
+ASM_predictions_by_country <- ASM_predictions_by_country %>% 
   spread(., key = prediction, value = n) %>% #transform table for country column
   mutate(total = yes + no) %>% #calculate total names per country
   mutate(percent = get_percent(no, total)) %>% #calculate per country % na-gendered
-  mutate(impact = round(((percent - percent_unpredicted)*(total/num_obs))/percent_unpredicted, digits = 4)) #calculate per country impact on overall na-gendered %
+  mutate(impact = round(((percent - ASM_percent_unpredicted)*(total/ASM_num_obs))/ASM_percent_unpredicted, digits = 4)) #calculate per country impact on overall na-gendered %
 
-top_five_num <- list_to_sent(df = predictions_by_country, 
-                             sort = "total", n = 5, pull = "country")
-  
-top_five_na <- list_to_sent(df = predictions_by_country, 
-                            sort = "percent", n = 5, pull = "country")
+ASM_top_five_num <- list_to_sent(df = ASM_predictions_by_country, 
+                                 sort = "total", n = 5, pull = "country")
 
-max_names_five_na <- predictions_by_country %>% arrange(desc(percent)) %>% 
+ASM_top_five_na <- list_to_sent(df = ASM_predictions_by_country, 
+                                sort = "percent", n = 5, pull = "country")
+
+ASM_max_names_five_na <- ASM_predictions_by_country %>% arrange(desc(percent)) %>% 
   head(n = 5) %>% summarise(max = max(total)) %>% unlist() %>% unname()
 
-min_names_five_na <- predictions_by_country %>% arrange(desc(percent)) %>% 
+ASM_min_names_five_na <- ASM_predictions_by_country %>% arrange(desc(percent)) %>% 
   head(n = 5) %>% summarise(min = min(total)) %>% unlist() %>% unname()
 
-top_five_impact <- predictions_by_country %>% filter(impact > 0) %>% 
+ASM_top_five_impact <- ASM_predictions_by_country %>% filter(impact > 0) %>% 
   list_to_sent(df = ., sort = "impact", n = 5, pull = "country") 
 
-plot_country_impact <- predictions_by_country %>% 
+plot_ASM_country_impact <- ASM_predictions_by_country %>% 
   filter(impact > 0) %>% #remove countries w. positive impact on prediction
   ggplot(., aes(x = reorder(country, impact), y = impact))+
   geom_col()+
   geom_text(aes(label = total), vjust = .5, hjust = -.15)+
-  coord_flip()+
+  coord_flip(ylim = c(0.0, 0.25))+
   labs(x = "Publication Country", y = "Negative Impact on Overall Gender Prediction",
        caption = "Number indicates total number of names associated with the country")+
   my_theme_horiz
 
-ggsave("Figure_S8.png", plot = plot_country_impact, device = 'png', 
-       path = '../submission', width = 12, height = 9)
+plot_grid(equ_plot, plot_ASM_country_impact, rel_heights = c(0.5, 2),
+          nrow = 2, labels = c('A', 'B'), label_size = 18)
+
+ggsave("Figure_S8.png", device = 'png', 
+       path = '../submission', width = 8, height = 10)
 
 #which countries have the most "unknown" gender names?
 #na_by_country <- na_obs %>%
