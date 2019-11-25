0bad06accf080fe89284e42c77f7b008b4f8b90e
akhagan <akhagan@umich.edu>
2019-11-25 16:08:28 -0500

    add unknown group & numbers

--- code/representation/US_inst_type_stats.R
+++ code/representation/US_inst_type_stats.R
@@ -1,7 +1,7 @@
 #gender split by U.S. institution type for authors vs reviewers vs editors:
 
 inst_stats_data <- data %>% 
-  filter(gender != "none") %>% 
+  #filter(gender != "none") %>% 
   mutate(role = fct_collapse(role,
                              "reviewer" = c("reviewer", "potential.reviewer"),
                              "editor" = c("editor", "senior.editor"))) %>% 
@@ -16,17 +16,29 @@ gender_n <- inst_stats_data %>%
   
 roles <- c("author", "reviewer", "editor")
 
-summ_US_stats <- inst_stats_data %>% 
+n_US_stats <- inst_stats_data %>% 
   group_by(role, gender, US.inst.type) %>% 
   summarise(n = n()) %>% 
   left_join(., gender_n, by = c("role", "gender")) %>% 
-  spread(key = US.inst.type, value = n) %>% 
+  spread(key = US.inst.type, value = n)
+
+percent_US_stats <- n_US_stats %>% 
   mutate_at(vars(`R1 Univ`:`Non-US Inst`), 
          list(~ get_percent(., total))
     ) %>% 
   select(-total) %>% 
   na.omit() %>% 
   gather(`R1 Univ`:`Non-US Inst`, 
-         key = US.inst.type, value = percent) 
+         key = US.inst.type, value = percent)
+
+summ_US_stats <- n_US_stats %>% 
+  select(-total) %>% 
+  na.omit() %>% 
+  gather(`R1 Univ`:`Non-US Inst`, 
+         key = US.inst.type, value = n) %>% 
+  left_join(., percent_US_stats, by = c("US.inst.type", "role", "gender"))
 
 summ_US_stats$role <- fct_relevel(summ_US_stats$role, roles)
+
+summ_US_stats$US.inst.type <- fct_relevel(summ_US_stats$US.inst.type, 
+                                          op_inst_list)
