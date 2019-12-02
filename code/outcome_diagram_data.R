b674fc698c9dae592ea8d8425a3ab5bb37c2eff8
akhagan <akhagan@umich.edu>
2019-12-02 17:16:51 -0500

    get updated numbers

--- code/outcome_diagram_data.R
+++ code/outcome_diagram_data.R
@@ -1,5 +1,7 @@
 outcomes <- data %>% 
-  select(grouped.random, published, grouped.vers, num.authors, num.versions) %>% 
+  select(grouped.random, published, grouped.vers, num.authors, num.versions, review.start, EJP.decision) %>% 
+  filter(EJP.decision %in% c("Accept", "Reject", "Revise")) %>% 
+  filter(grouped.vers == 1) %>% 
   distinct() 
 
 manus <- outcomes %>% pull(grouped.random) %>% unique()
@@ -18,35 +20,56 @@ vers_auth_summary <- final_outcome %>%
             med.auth = median(num.authors), 
             iqr.vers = IQR(num.versions), iqr.auth = IQR(num.authors))
 
-first_descision <- data %>% 
+#outcome of manuscripts after editor's first review
+editor_descision <- data %>%  
   filter(grouped.vers == 1) %>% 
+  filter(grouped.random %in% manus) %>% 
+  filter(is.na(review.start)) %>% 
+  select(grouped.random, EJP.decision, role, 
+         random.person.id, gender) %>% 
+  filter(EJP.decision %in% c("Accept", "Revise", "Reject")) %>% 
+  distinct()
+
+#first decision database
+first_descision <- data %>%  
+  filter(grouped.vers == 1) %>% 
+  filter(grouped.random %in% manus) %>%
   filter(!is.na(review.start)) %>% 
   select(grouped.random, EJP.decision, role, 
          random.person.id, gender) %>% 
   filter(EJP.decision %in% c("Accept", "Revise", "Reject")) %>% 
   distinct()
 
-genders <- first_descision %>% 
-  group_by(EJP.decision, gender) %>% 
-  summarise(n = n())
+#identify duplicate
+editor_should <- anti_join(final_outcome, first_descision, 
+                           by = "grouped.random") %>% 
+  select(grouped.random) %>% distinct()
 
-decisions <- first_descision %>% 
-  select(EJP.decision, grouped.random) %>% 
-  distinct() %>% 
-  group_by(EJP.decision) %>% summarise(n = n())
+extra_ed <- anti_join(editor_descision, editor_should, 
+                      by = "grouped.random") %>% 
+  select(grouped.random) %>% 
+  distinct() %>% pull()
 
-editor_descision <- data %>% 
-  filter(grouped.vers == 1) %>% 
-  filter(is.na(review.start)) %>% 
-  select(grouped.random, EJP.decision, role, random.person.id, gender) %>% 
-  filter(EJP.decision %in% c("Accept", "Revise", "Reject")) %>% 
-  distinct()
+#update editor decisions to remove duplicated manuscripts
+editor_descision <- editor_descision %>% 
+  filter(grouped.random %not_in% extra_ed)
 
+#outcome of manuscripts after first review by editor
 ed_des_genders <- editor_descision %>% 
   group_by(EJP.decision, gender) %>% 
   summarise(n = n())
 
 ed_decisions <- editor_descision %>% 
+  select(EJP.decision, grouped.random) %>% 
+  distinct() %>% 
+  group_by(EJP.decision) %>% summarise(n = n())
+
+#outcome of manuscripts after first round of review
+genders <- first_descision %>% 
+  group_by(EJP.decision, gender) %>% 
+  summarise(n = n())
+
+decisions <- first_descision %>% 
   select(EJP.decision, grouped.random) %>% 
   distinct() %>% 
   group_by(EJP.decision) %>% summarise(n = n())
